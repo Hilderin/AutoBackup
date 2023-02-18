a1cfc93cafc4e2592248fc3f17cd4e29ceaafb2c
Hilderin <81109165+Hilderin@users.noreply.github.com>
2023-02-18 10:10:17 -0500

    Add log AppData + new version 1.0.1

--- AutoBackup/Utils.cs
+++ AutoBackup/Utils.cs
@@ -18,6 +18,12 @@ namespace AutoBackup
         private static string _logPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "AutoBackup.log");
 
         /// <summary>
+        /// Path du log dans le AppData
+        /// </summary>
+        private static string _logPathAppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AutoBackup\\AutoBackup.log");
+
+
+        /// <summary>
         /// Log on disk
         /// </summary>
         public static void Log(string data)
@@ -25,6 +31,12 @@ namespace AutoBackup
             try
             {
                 File.AppendAllText(_logPath, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " " + data + Environment.NewLine);
+            }
+            catch { }
+
+            try
+            {
+                File.AppendAllText(_logPathAppData, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " " + data + Environment.NewLine);
             }
             catch { }
         }
